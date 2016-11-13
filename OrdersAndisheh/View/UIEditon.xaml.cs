ae748e6a25bad1e022dbbc6d9fb9c7a80449a8a3
Home <pc2man@gmail.com>
2016-11-13 22:03:46 +0330

    656565888

---
+++ OrdersAndisheh/View/UIEditon.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+
+namespace OrdersAndisheh.View
+{
+    /// <summary>
+    /// Description for UIEditon.
+    /// </summary>
+    public partial class UIEditon : Window
+    {
+        /// <summary>
+        /// Initializes a new instance of the UIEditon class.
+        /// </summary>
+        public UIEditon()
+        {
+            InitializeComponent();
+        }
+    }
+}
