d33797f7babdd48f74202afd108f8819ef2fa5e6
Home <pc2man@gmail.com>
2016-11-22 22:27:07 +0330

    6545

--- OrdersAndisheh/BL/Container.cs
+++ OrdersAndisheh/BL/Container.cs
@@ -2,16 +2,35 @@
 using OrdersAndisheh.DBL;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace OrdersAndisheh.BL
 {
-    public class Container
+    public class Container : INotifyPropertyChanged
     {
+        //اینجا یه کانتینر شامل یک یا یکسری اقلام میشه
+        //که تعداد جایگاهش بیشتر از 8 نیست اگر وزنش بیشتر از تک نشه 
+        //و یه سری پراپرتی برای نمایش وزن و جایگاه و غیره
+        // در ضمن میتونه اینجا بگه راننده در چه حده خاوری و غیره
+
+        public Container()
+        {
+            items = new List<ItemSefaresh>();
+            driver = new Driver();
+
+        }
+
+        public Container(List<ItemSefaresh> items)
+            : this()
+        {
+            this.items = items;
+        }
        
         public Container(List<ItemSefaresh> items,Driver driver)
+            :this()
         {
             this.items = items;
             this.driver = driver;
@@ -22,18 +41,36 @@ namespace OrdersAndisheh.BL
         public List<ItemSefaresh> items
         {
             get { return _items; }
-            set { _items = value; }
+            set 
+            { 
+                _items = value;
+                NotifyPropertyChanged("items");
+            }
         }
 
-        private int myVar;
+        private Driver _driver;
 
-        public int MyProperty
+        public Driver driver
         {
-            get { return myVar; }
-            set { myVar = value; }
+            get { return _driver; }
+            set 
+            { 
+                _driver = value;
+                NotifyPropertyChanged("driver");
+            }
         }
-        
-        
 
+
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged(String info)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(info));
+            }
+        }
     }
 }
---
+++ OrdersAndisheh/BL/ContainerManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrdersAndisheh.BL
+{
+    class ContainerManager
+    {
+        //اینجا میایم کل آیتم سفارش اون رو رو میگیریم
+        //بعد کانتینر میسازیم
+        //کانتینر میتونه ایتم های سایپایی باشه
+        //تا ایتم فقط غربیلک 206
+        //اینجا دقیقا با توجه به وزن و تعداد پالت میاد یه سری کانینر پیشنهاد میده 
+        //
+    }
+}
