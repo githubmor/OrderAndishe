5ccc05d52c09fcc5bc3efcd6d966513c60ad7aae
githubmor <pc2man@gmail.com>
2017-11-05 08:19:42 +0330

    655

---
+++ OrdersAndisheh/BL/Importing/FileImporter.cs
@@ -0,0 +1,35 @@
+﻿using OfficeOpenXml;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrdersAndisheh.BL.Importing
+{
+    public class FileImporter
+    {
+        protected ExcelWorksheet WorkSheet;
+        public FileImporter(string path)
+        {
+            FileInfo f = new FileInfo(path);
+            ExcelPackage package = new ExcelPackage(f);
+            WorkSheet = package.Workbook.Worksheets[1];
+            //FileAddress = f.FullName;
+            //FileName = f.Name;
+            //ColumnCount = WorkSheet.Dimension.Columns;
+            //start = WorkSheet.Dimension.Start;
+            //end = WorkSheet.Dimension.End;
+        }
+        public ImportDataKind GetSample()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IBaseImportData GetImportdata()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ OrdersAndisheh/BL/Importing/IBaseImportData.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrdersAndisheh.BL.Importing
+{
+    public interface IBaseImportData
+    {
+        string KalaName { get; set; }
+        string CodeKala { get; set; }
+        string FaniCode { get; set; }
+        string CodeJense { get; set; }
+        string Tedad { get; set; }
+        string Maghsad { get; set; }
+        string Ranande { get; set; }
+        string Pelak { get; set; }
+        string Car { get; set; }
+
+        //public List<string> GetFieldEnum()
+        //{
+        //    var t = "";
+        //    foreach (PropertyInfo prop in typeof(IBaseImportData).GetProperties())
+        //    {
+        //        t = t + prop;
+        //    }
+
+        //    return t;
+        //}
+    }
+}
